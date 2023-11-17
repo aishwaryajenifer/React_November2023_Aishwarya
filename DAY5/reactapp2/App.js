0ee0ecf56f89599b1a45701dc1fcafe8f00f9ef9
aishwaryajenifer <124856008+aishwaryajenifer@users.noreply.github.com>
2023-11-17 11:47:44 +0530

    DAY5

---
+++ DAY5/reactapp2/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Funlifecycle from './components/Funclifecycle';
+function App() {
+  return (
+    <div className="App">
+      <Funlifecycle/>
+    </div>
+  );
+}
+
+export default App;
